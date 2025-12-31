f3ad913a3339dd3b0981194fe5e6ab42b1f9dd37
pedrxbro <pedrohdpaula@edu.univali.br>
2025-12-31 16:09:10 -0300

    chore(infra): provision rds postgres16 with managed secret

---
+++ infra/03-data-rds-stack/output.tf
@@ -0,0 +1,19 @@
+output "db_endpoint" {
+  value = aws_db_instance.this.address
+}
+
+output "db_port" {
+  value = aws_db_instance.this.port
+}
+
+output "db_name" {
+  value = aws_db_instance.this.db_name
+}
+
+output "db_username" {
+  value = aws_db_instance.this.username
+}
+
+output "db_secret_arn" {
+  value = aws_db_instance.this.master_user_secret[0].secret_arn
+}
